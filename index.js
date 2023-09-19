c4703ced1b16c9aba5407f3e2451abb15b0e9d8e
riddhishwar <82893678+riddhishwar@users.noreply.github.com>
2023-09-19 16:23:27 +0530

    final commit

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 // server.js
 import express from 'express';
 import cors from 'cors';
-import helmet from 'helmet';
 import bodyParser from 'body-parser';
 import fs from 'fs';
 
 const app = express();
 app.use(cors());
-app.use(helmet());
 app.use(bodyParser.json());
 
 
